48d004f9d5e97c804b89438fc14ee8e14d4afa06
graborg <intemicke@gmail.com>
2013-04-26 12:02:57 +0200

    build HTTP response

--- src/Server.java
+++ src/Server.java
@@ -12,56 +12,58 @@ import java.nio.charset.CharsetEncoder;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-
 public class Server {
-	
+
 	private int port;
-	
-	public static void main(String args[]){
-        int port = 8060;
-        new Server(port);
+
+	public static void main(String args[]) {
+		int port = 8060;
+		new Server(port);
 	}
-	
-	public Server(int port){
+
+	public Server(int port) {
 		this.port = port;
 		AsynchronousChannelGroup group;
-		
-        try {
-        	
-        	group = AsynchronousChannelGroup.withThreadPool(Executors
-                    .newSingleThreadExecutor());
-        	
-            final AsynchronousServerSocketChannel serverSocket = AsynchronousServerSocketChannel.open(group);
-            serverSocket.bind(new InetSocketAddress(this.port));
 
+		try {
 
-            serverSocket.accept("Client connection", 
-                    new CompletionHandler<AsynchronousSocketChannel, Object>() {
-                public void completed(AsynchronousSocketChannel ch, Object att) {
-                    System.out.println("Accepted a connection");
+			group = AsynchronousChannelGroup.withThreadPool(Executors
+					.newSingleThreadExecutor());
 
-                    // accept the next connection
-                    serverSocket.accept("Client connection", this);
+			final AsynchronousServerSocketChannel serverSocket = AsynchronousServerSocketChannel
+					.open(group);
+			serverSocket.bind(new InetSocketAddress(this.port));
+			
+			
+			CompletionHandler<AsynchronousSocketChannel, Object> handler = new CompletionHandler<AsynchronousSocketChannel, Object>() {
+				public void completed(AsynchronousSocketChannel ch,
+						Object att) {
+					System.out.println("Accepted a connection");
 
-                    // handle this connection
-                    handleConnection(ch);
-                }
+					// accept the next connection
+					serverSocket.accept("Client connection", this);
 
-                public void failed(Throwable exc, Object att) {
-                    System.out.println("Failed to accept connection");
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-        
-        
-        System.out.println("Server accepting conenctions on port " + port);
-        
-        
-        // wait until group.shutdown()/shutdownNow(), or the thread is interrupted:
-        try {
+					// handle this connection
+					handleConnection(ch);
+				}
+
+				public void failed(Throwable exc, Object att) {
+					System.out.println("Failed to accept connection");
+				}
+			};
+
+			serverSocket.accept("Client connection",handler);
+					
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println("Server accepting conenctions on port " + port);
+
+		// wait until group.shutdown()/shutdownNow(), or the thread is
+		// interrupted:
+		try {
 			group.awaitTermination(Long.MAX_VALUE, TimeUnit.HOURS);
 		} catch (InterruptedException e) {
 			System.out.println("Shutting down");
@@ -69,9 +71,16 @@ public class Server {
 		}
 
 	}
-	
-	protected void handleConnection(AsynchronousSocketChannel ch){
-		String response = "Buy more ฿itcoins!\n";
+
+	protected void handleConnection(AsynchronousSocketChannel ch) {
+//		ByteBuffer buffer =
+//		ch.read(buffer);
+		String response = "HTTP/1.1 200 OK\n" +
+				"Content-Type: text/html\n" +
+				"Server: MMPJ\n" +
+				"Connection: close\n" +
+				"\n" +
+				"<h1 style='color:red;'>bitcoinZ</h1>\n";
 		
 		CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
 		ByteBuffer buf;
